bce37b1654a467df90299696a44e46417ea735fd
sawapak-B <71916196+sawapak-B@users.noreply.github.com>
2020-09-26 16:01:08 +0700

    Add files via upload

---
+++ Exam002_ArrayList/Exam002_ArrayList/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Exam002_ArrayList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.btShow = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btShow);
+            this.groupBox1.Controls.Add(this.btDelete);
+            this.groupBox1.Controls.Add(this.btAdd);
+            this.groupBox1.Controls.Add(this.txtInput);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(18, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(494, 215);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Array list";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input Data :";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(165, 57);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(258, 28);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(69, 142);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(99, 37);
+            this.btAdd.TabIndex = 2;
+            this.btAdd.Text = "Add";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // btDelete
+            // 
+            this.btDelete.Location = new System.Drawing.Point(191, 142);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(99, 37);
+            this.btDelete.TabIndex = 2;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // btShow
+            // 
+            this.btShow.Location = new System.Drawing.Point(324, 142);
+            this.btShow.Name = "btShow";
+            this.btShow.Size = new System.Drawing.Size(99, 37);
+            this.btShow.TabIndex = 2;
+            this.btShow.Text = "Show All";
+            this.btShow.UseVisualStyleBackColor = true;
+            this.btShow.Click += new System.EventHandler(this.btShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 256);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btShow;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Exam002_ArrayList/Exam002_ArrayList/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Exam002_ArrayList
+{
+    public partial class Form1 : Form
+    {
+
+        ArrayList Lists = new ArrayList(); 
+
+        public Form1()
+        {
+            InitializeComponent();
+            Lists = new ArrayList();
+        }
+
+      
+        
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                if (txtInput.Text == "")
+                {
+                    MessageBox.Show("กรุณากรอกข้อมูล");
+                }
+            }
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            if (txtInput.Text != "")
+            {
+                Lists.Add(txtInput.Text);
+                txtInput.Text = "";
+            }
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            Lists.Remove(txtInput.Text);
+            txtInput.Text = "";
+        }
+
+        private void btShow_Click(object sender, EventArgs e)
+        {
+            int i;
+
+            for (i = 0; i < Lists.Count; i++)
+            {
+                MessageBox.Show(Lists[i].ToString());
+            }
+
+        }
+    }
+}
